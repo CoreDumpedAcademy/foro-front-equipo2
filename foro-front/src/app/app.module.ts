e48ba6fb6904d152ff14387a974053349bc06f02
ec-dani <jd.cantuna@alumnos.upm.es>
2019-12-11 22:57:11 +0100

    registro added

--- foro-front/src/app/app.module.ts
+++ foro-front/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RegisterComponent } from './components/user/register/register.component';
@@ -9,6 +9,7 @@ import { PostComponent } from './components/posts/post/post.component';
 import { PostResultsComponent } from './components/posts/post-results/post-results.component';
 import { PostTopicComponent } from './components/posts/post-topic/post-topic.component';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,8 @@ import { HomePageComponent } from './components/home-page/home-page.component';
     HomePageComponent
   ],
   imports: [
+    FormsModule,
+    HttpClientModule,
     BrowserModule,
     AppRoutingModule
   ],
