8fbefa195c876e9f103ae0b19c77cf693871e21c
ec-dani <jd.cantuna@alumnos.upm.es>
2019-12-19 12:36:05 +0100

    navigate from register to home-page

--- foro-front/src/app/components/user/register/register.component.ts
+++ foro-front/src/app/components/user/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpErrorResponse } from '@angular/common/http';
 import { RoutesService  } from '../../../service/routes.service';
+import { CookieService } from 'ngx-cookie-service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +13,8 @@ export class RegisterComponent implements OnInit {
 
   constructor(
     private api: RoutesService,
+    private router: Router,
+    private cookieService: CookieService,
   ) { }
 
   ngOnInit() {
@@ -19,9 +23,12 @@ export class RegisterComponent implements OnInit {
 
   register(data) {
     console.log(data.value)
-    this.api.registerUser(data.value).subscribe((response) => {
-      console.log(data.value);
+    this.api.registerUser(data.value).subscribe((response: { token: string }) => {
+      console.log(response.token);
+      this.cookieService.set('token', response.token);
+      this.api.token(response.token);
       alert('usuario se ha guardado correctamente');
+      window.location.reload();
     }, (error: HttpErrorResponse) => {
       alert(error.error.message);
     });
